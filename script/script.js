e6d9ff8af34930acc0085af2c54bdb68ccc95f4e
fatou fall <fatoufall95@gmail.com>
2024-11-09 22:28:35 -0500

    modify the javascript

--- script/script.js
+++ script/script.js
@@ -3,50 +3,26 @@
 confirm("are you ready to go on vacation?");
 
 const itemInput = document.querySelector("#itemInput");
-const additemsBtns = document.querySelector("#addItemBtn");
+const addItemBtn = document.querySelector("#addItemBtn");
+const itemList = document.querySelector("#itemList");
+const filterOption = document.querySelector("#itemSelection");
 
-// // Caching DOM Elements
-// // Caching DOM Elements
-// const cList = document.getElementById("itemList");
-// const iInput = document.getElementById("item-Input");
-// const cBtn = document.getElementById("addItemBtn");
+addItemBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  addItem();
+});
+function addItem() {
+  const itemText = itemInput.value.trim();
+  if (itemText) {
+    // Create list item element
+    const listItem = document.createElement("li");
+    listItem.classList.add("itemThings");
+    listItem.textContent = itemText;
 
-// console.log(iInput);
+    // Append the item to the list
+    itemList.appendChild(listItem);
 
-// // cinput.addEventListener('input', (event) => {
-// //     console.log(event.target.value);
-// // })
-
-// // let i = 0;
-// // create an event handler - a function that executes in response to the event
-// function addComment(evt) {
-//   // console.log(evt)
-//   console.log(evt.target);
-//   // console.log(iInput.value);
-//   const newComment = iInput.value;
-//   console.log(newComment);
-//   // console.log('i can output anything', ++i);
-
-//   // This wasn't in the instructions, but
-//   // we added a guard against empty comments
-//   if (newComment.trim() === "") return;
-
-//   // add the value to the comment list element
-//   const newLi = document.createElement("li");
-//   newLi.textContent = newComment;
-//   cList.appendChild(newLi);
-
-//   // clear the input element
-//   importInput.value = "";
-
-//   // focus the input element
-//   iInput.focus();
-
-//   // you can add an event listener
-//   // adding this here wouldn't make a lot of sense
-//   // but you can see it working
-//   cBtn.removeEventListener("click", addComment);
-// }
-
-// // the event that we are looking for is the button click
-// cBtn.addEventListener("click", addComment);
+    // Clear the input field
+    itemInput.value = "";
+  }
+}
